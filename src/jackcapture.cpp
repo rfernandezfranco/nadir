e52d61fa5e4b432b240ab7a07221c7c8529957a2
Juan Roldan Ruiz <juan@shoden.es>
2011-11-25 22:28:17 +0100

    headers

--- src/jackcapture.cpp
+++ src/jackcapture.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2009-2011 Juan Roldan Ruiz <juan.roldan@gmail.com>
+ *  
+ * This file is part of Nadir.
+ * 
+ * Nadir is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nadir is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nadir.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #include "jackcapture.h"
 #include "config.h"
 
